b2914117a91c597bb87cef7b53c4c167efecf200
Assadujjaman Tilok <72868585+AssadujjamanTilok@users.noreply.github.com>
2023-05-05 07:52:08 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<windows.h>
+#include<mmsystem.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==2){
+        PlaySound(TEXT("sound.wav"),NULL,SND_SYNC);
+        }
+    else{
+        printf("Yes");
+    }
+    return 0;
+}
